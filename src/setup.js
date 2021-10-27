0fda2b4a5fcacf7f87052f7a42cd6cba6fc99fb1
Udhay <57910021+code123841@users.noreply.github.com>
2021-10-27 20:50:31 +0530

    Update setup.js

--- src/setup.js
+++ src/setup.js
@@ -1,3 +1,11 @@
 export async function setup (){
     console.log('Setting up the files... For the notes app')
+    const exec = require('child_process').exec;  
+        exec('my.bat', (err, stdout, stderr) => {  
+        if (err) {  
+            console.error(err);  
+            return;  
+        }  
+        console.log(stdout);  
+    });  
 }
